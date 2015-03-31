6fb9833da1131eb76dbe214292130eb92a7a3890
ralphitlei <ralphitlei@gmail.com>
2015-03-31 20:56:06 +1100

    updated to include alert

--- phonegap-workshop-master/phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/phonegap-workshop-master/js/main.js
@@ -13,8 +13,19 @@ var app = {
         });
     },
 
-    initialize: function() {
-        this.store = new WebSqlStore();
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
+    initialize: function () {
+        var self = this;
+        this.store = new MemoryStore(function () {
+            self.showAlert('Store Initialized', 'info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
